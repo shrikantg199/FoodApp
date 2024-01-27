db2ee58b41a9df0d51f9c8737be4dcc213e696f1
shrikant gaikwad <shrikantg199@gmail.com>
2024-01-27 20:32:40 +0530

    initial commit

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,20 +1,23 @@
 function Category() {
   return (
     <div className="flex gap-4 mx-4 mt-6 mb-12 overflow-x-scroll lg:m-8 lg:overflow-x-hidden scroll-smooth scroll ">
-      <button className="bg-[#fc8019] px-4 rounded-lg py-1  text-lg text-white font-medium ">
+      <button className="bg-[#fc8019] px-4 rounded-lg  lg: py-1  text-lg text-white font-medium ">
         All
       </button>
-      <button className="bg-[#fc8019] px-4 py-1 rounded-lg text-md text-white font-medium">
-        Lunch
+      <button className="bg-[#fc8019] px-4  lg: py-1 rounded-lg text-md text-white font-medium">
+        Pizza
       </button>
-      <button className="bg-[#fc8019] px-4 rounded-lg py-1  text-md text-white font-medium ">
-        Breakfast
+      <button className="bg-[#fc8019] px-4 rounded-lg  lg: py-1  text-md text-white font-medium ">
+        Healthy Foods
       </button>
-      <button className="bg-[#fc8019] px-4 rounded-lg py-1  text-md text-white font-medium">
-        Dinner
+      <button className="bg-[#fc8019] px-4 rounded-lg  lg: py-1  text-md text-white font-medium">
+        Burger
       </button>
-      <button className="bg-[#fc8019] px-4 rounded-lg py-1  text-md text-white font-medium">
-        Dinner
+      <button className="bg-[#fc8019] px-4 rounded-lg  lg: py-1  text-md text-white font-medium">
+        Ice Cream
+      </button>
+      <button className="bg-[#fc8019] px-4 rounded-lg  lg: py-1  text-md text-white font-medium">
+        Coffee
       </button>
     </div>
   );
