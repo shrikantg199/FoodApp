265f58f249ce3a93eae826029141c3c8af529e7b
shrikant gaikwad <shrikantg199@gmail.com>
2024-01-29 17:11:11 +0530

    initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { NextUIProvider } from "@nextui-org/react";
-
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <NextUIProvider>
-      <App />
-    </NextUIProvider>
+    <Provider store={store}>
+      <NextUIProvider>
+        <App />
+      </NextUIProvider>
+    </Provider>
   </React.StrictMode>
 );
