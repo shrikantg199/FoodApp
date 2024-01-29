f5201f34790ea0efddcea8b7b996fc6e277a89bc
shrikant gaikwad <shrikantg199@gmail.com>
2024-01-29 17:08:10 +0530

    initial commit

--- src/components/Foodcards.jsx
+++ src/components/Foodcards.jsx
@@ -1,6 +1,11 @@
 import { TiStarFullOutline } from "react-icons/ti";
-
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slices/CartSlice";
 function Foodcards({ img, rating, category, desc, price, id, name }) {
+  const dispatch = useDispatch();
+  const handleAddToCart = () => {
+    dispatch(addToCart({ id, name, img, price, rating, qty: 1 }));
+  };
   return (
     <>
       <div className="mx-4 w-[250px] rounded-3xl bg-white p-5 flex  mb-8 flex-col">
@@ -20,7 +25,10 @@ function Foodcards({ img, rating, category, desc, price, id, name }) {
             <TiStarFullOutline className="text-xl text-yellow-300" />
           </span>
           {rating}
-          <button className="px-2 py-1 ml-12 text-white bg-green-500 rounded-lg">
+          <button
+            onClick={handleAddToCart}
+            className="px-2 py-1 ml-12 text-white bg-green-500 rounded-lg"
+          >
             {" "}
             Add to cart
           </button>
