0d7714b962ebc2cf34f66661b95323975ed9c7b7
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-05 17:40:54 +0530

    initial commit

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -1,25 +1,37 @@
 import React from "react";
 import Foodcards from "./Foodcards";
 import FoodData from "./FoodData";
-function FoodItems() {
-  return (
-    <div className="flex flex-wrap justify-center gap-4 lg:justify-start">
-      {FoodData.map((food) => {
-        return (
-          <Foodcards
-            key={food}
-            name={food.name}
-            id={food.id}
-            img={food.img}
-            rating={food.rating}
-            category={food.category}
-            desc={food.desc}
-            price={food.price}
-          />
-        );
-      })}
-    </div>
-  );
-}
+import toast, { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
+const FoodItems = () => {
+  const category = useSelector((state) => state.category.category);
+
+  const handleToast = (name) => toast.success(`Added ${name} `);
+  return (
+    <>
+      <Toaster position="top-center" reverseOrder={false} />
+      <div className="flex flex-wrap gap-10 justify-center lg:justify-start mx-6 my-10">
+        {FoodData.filter((food) => {
+          if (category === "All") {
+            return food;
+          } else {
+            return category === food.category;
+          }
+        }).map((food) => (
+          <Foodcards
+            key={food.id}
+            id={food.id}
+            name={food.name}
+            price={food.price}
+            desc={food.desc}
+            rating={food.rating}
+            img={food.img}
+            handleToast={handleToast}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 export default FoodItems;
