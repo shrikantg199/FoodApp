dfccb34af20d30eddac6916c81e0ea178fa90baf
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-12 15:12:00 +0530

    initial commit

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -3,7 +3,7 @@ import Foodcards from "./Foodcards";
 import FoodData from "./FoodData";
 import toast, { Toaster } from "react-hot-toast";
 import { useSelector } from "react-redux";
-
+import logo from "../assets/Mask Group.png";
 const FoodItems = () => {
   const category = useSelector((state) => state.category.category);
   const search = useSelector((state) => state.search.search);
@@ -34,6 +34,66 @@ const FoodItems = () => {
           />
         ))}
       </div>
+
+      <footer class="p-4 bg-white md:p-8 lg:p-2 dark:bg-gray-800">
+        <div class="mx-auto max-w-screen-xl text-center">
+          <a
+            href="#"
+            class="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img src={logo} alt="" width="20" className="m-3" />
+            FoodApp
+          </a>
+          <p class="my-6 text-gray-500 dark:text-gray-400">
+            Flavors at Your Fingertips: Unleashing Culinary Delights with Every
+            Swipe.
+          </p>
+          <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                About
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">
+                Premium
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6 ">
+                Campaigns
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">
+                Blog
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">
+                Affiliate Program
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">
+                FAQs
+              </a>
+            </li>
+            <li>
+              <a href="#" class="mr-4 hover:underline md:mr-6">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2023-2024{" "}
+            <a href="#" class="hover:underline">
+              FoodApp
+            </a>
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
     </>
   );
 };
