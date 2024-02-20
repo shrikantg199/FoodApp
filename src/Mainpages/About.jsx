b6be22cd49a179b1fb248a9934d3160e856770d6
shrikant gaikwad <shrikantg199@gmail.com>
2024-02-20 13:44:25 +0530

    initial commit

--- src/Mainpages/About.jsx
+++ src/Mainpages/About.jsx
@@ -1,24 +1,26 @@
-<div>
-  import React from "react"; import photo from "../assets/foodapppic.jpeg";
-  import Navbar from "../components/Navbar"; import {"{"} Link {"}"} from
-  "react-router-dom"; const About = () =&gt; {"{"}
+import React from "react";
+import photo from "../assets/foodapppic.jpeg";
+import Navbar from "../components/Navbar";
+import { Link } from "react-router-dom";
+const About = () => {
   return (
-  <div>
-    <navbar>
-      <div className="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-6xl">
-        <div className="w-full h-96 lg:w-[100vh] lg:h-[80vh]">
+    <div>
+      <Navbar />
+      <div class="relative flex flex-col items-center mx-auto lg:flex-row-reverse lg:max-w-5xl lg:mt-12 xl:max-w-6xl">
+        <div class="w-full h-96 lg:w-[100vh] lg:h-[80vh]">
           <img
-            className="h-full w-full object-cover"
-            src="{photo}"
+            class="h-full w-full object-cover"
+            src={photo}
             alt="Winding mountain road"
           />
         </div>
-        <div className="max-w-lg bg-white md:max-w-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-1/2 lg:left-0 lg:mt-20  xl:mt-24">
-          <div className="flex flex-col p-8 md:px-8">
-            <h2 className="text-2xl font-medium uppercase text-green-800 lg:text-4xl">
+
+        <div class="max-w-lg bg-white md:max-w-2xl md:z-10 md:shadow-lg md:absolute md:top-0 md:mt-48 lg:w-1/2 lg:left-0 lg:mt-20  xl:mt-24">
+          <div class="flex flex-col p-8 md:px-8">
+            <h2 class="text-2xl font-medium uppercase text-green-800 lg:text-4xl">
               Food App
             </h2>
-            <p className="mt-4">
+            <p class="mt-4">
               Introducing our cutting-edge food delivery app, delivering
               culinary delights to your doorstep with just a tap. Experience a
               seamless journey from menu exploration to doorstep delivery, with
@@ -28,18 +30,19 @@
               where flavor meets technology, creating a gastronomic adventure at
               your fingertips. Savor the convenience, savor the moment.
             </p>
-            <div className="mt-8">
-              <link
+            <div class="mt-8">
+              <Link
                 to="/"
-                className="inline-block rounded-lg w-full text-center text-lg font-medium text-gray-100 bg-green-600 border-solid border-2 border-gray-600 py-4 px-10 hover:bg-green-800 hover:shadow-md md:w-48"
-              />
-              Order Food
+                class="inline-block rounded-lg w-full text-center text-lg font-medium text-gray-100 bg-green-600 border-solid border-2 border-gray-600 py-4 px-10 hover:bg-green-800 hover:shadow-md md:w-48"
+              >
+                Order Food
+              </Link>
             </div>
           </div>
         </div>
       </div>
-    </navbar>
-  </div>
+    </div>
   );
-  {"}"}; export default About;
-</div>;
+};
+
+export default About;
--- src/Mainpages/Contact.jsx
+++ src/Mainpages/Contact.jsx
@@ -1,84 +1,92 @@
-<div>
-  import React from "react"; import {"{"} Input {"}"} from "@nextui-org/react";
-  import Navbar from "../components/Navbar"; const Contact = () =&gt; {"{"}
-  const variants = ["bordered"]; return (
-  <div name="contact" classname="w-full h-screen    bg-white">
-    <navbar>
-      <div classname="flex flex-col justify-center h-full max-w-screen-lg mx-auto  ">
-        <div classname="pb-8 ">
-          <p classname=" text-4xl text-black font-bold  border-gray-500 text-center">
+import React from "react";
+import { Input } from "@nextui-org/react";
+import Navbar from "../components/Navbar";
+const Contact = () => {
+  const variants = ["bordered"];
+
+  return (
+    <div name="contact" className="w-full h-screen    bg-white">
+      <Navbar />
+
+      <div className="flex flex-col justify-center h-full max-w-screen-lg mx-auto  ">
+        <div className="pb-8 ">
+          <p className=" text-4xl text-black font-bold  border-gray-500 text-center">
             Food Preferences
           </p>
-          <p classname="py-3 text-black text-center">
+          <p className="py-3 text-black text-center">
             Tell us about your food preferences below
           </p>
         </div>
-        <div classname="flex items-center justify-center">
+
+        <div className="flex items-center justify-center">
           <form
             action="https://example.com/submit-food-preferences"
             method="POST"
-            classname="flex flex-col w-full gap-2 md:w-1/2"
+            className="flex flex-col w-full gap-2 md:w-1/2"
           >
-            <div classname="flex flex-col w-full gap-4">
-              {"{"}variants.map((variant) =&gt; (
-              <div
-                key="{variant}"
-                classname="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
-              >
-                <input
-                  type="text"
-                  variant="{variant}"
-                  label="Name"
-                  classname="border-white"
-                />
-              </div>
-              )){"}"}
+            <div className="flex flex-col w-full gap-4">
+              {variants.map((variant) => (
+                <div
+                  key={variant}
+                  className="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
+                >
+                  <Input
+                    type="text"
+                    variant={variant}
+                    label="Name"
+                    className="border-white"
+                  />
+                </div>
+              ))}
             </div>
-            <div classname="flex flex-col w-full">
-              {"{"}variants.map((variant) =&gt; (
-              <div
-                key="{variant}"
-                classname="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
-              >
-                <input
-                  type="email"
-                  variant="{variant}"
-                  label="Email"
-                  classname="border-white"
-                />
-              </div>
-              )){"}"}
+
+            <div className="flex flex-col w-full">
+              {variants.map((variant) => (
+                <div
+                  key={variant}
+                  className="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
+                >
+                  <Input
+                    type="email"
+                    variant={variant}
+                    label="Email"
+                    className="border-white"
+                  />
+                </div>
+              ))}
             </div>
-            <div classname="flex flex-col w-full">
-              {"{"}variants.map((variant) =&gt; (
-              <div
-                key="{variant}"
-                classname="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
-              >
-                <input
-                  type="text"
-                  variant="{variant}"
-                  label="Favorite Cuisine"
-                  classname="border-white"
-                />
-              </div>
-              )){"}"}
+
+            <div className="flex flex-col w-full">
+              {variants.map((variant) => (
+                <div
+                  key={variant}
+                  className="flex flex-wrap w-full gap-4 mb-6 md:flex-nowrap md:mb-0"
+                >
+                  <Input
+                    type="text"
+                    variant={variant}
+                    label="Favorite Cuisine"
+                    className="border-white"
+                  />
+                </div>
+              ))}
             </div>
+
             <textarea
               name="message"
               placeholder="Additional Comments"
-              rows={10}
-              classname="p-2 text-white bg-transparent border-2 rounded-xl focus:outline-none"
-              defaultValue={""}
-            />
-            <button classname="flex items-center px-6 py-3 mx-auto my-8 text-white duration-300 rounded-md bg-gradient-to-b from-cyan-500 to-blue-500 hover:scale-110">
+              rows="10"
+              className="p-2 text-white bg-transparent border-2 rounded-xl focus:outline-none"
+            ></textarea>
+
+            <button className="flex items-center px-6 py-3 mx-auto my-8 text-white duration-300 rounded-md bg-gradient-to-b from-cyan-500 to-blue-500 hover:scale-110">
               Submit Preferences
             </button>
           </form>
         </div>
       </div>
-    </navbar>
-  </div>
+    </div>
   );
-  {"}"}; export default Contact;
-</div>;
+};
+
+export default Contact;
